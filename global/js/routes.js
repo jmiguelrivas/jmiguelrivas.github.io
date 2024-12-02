2086ea3f9ec33ef1e0788c0649974b0bf0934534
Miguel Rivas <github.5yeil@simplelogin.fr>
2024-12-02 12:52:39 -0800

    navigate to new route without loosing params

--- global/js/routes.js
+++ global/js/routes.js
@@ -3,6 +3,7 @@ const s2025 = [
     name: 'Home',
     url: '2025/index',
     icon: 'home',
+    hidden: true,
   },
   {
     name: 'Resume',
@@ -13,17 +14,22 @@ const s2025 = [
     name: 'Portfolio',
     url: '2025/portfolio',
     icon: 'book',
+    hidden: true,
   },
   {
     name: 'Grid System',
     url: '2025/nano',
     icon: 'phone',
+    hidden: true,
   },
 ]
 
 const sLOM = [
   {
-    name: 'LOM',
+    separator: true,
+  },
+  {
+    name: 'LOM Merged Servers Timeline',
     url: 'lom/index',
     icon: 'lock',
     hidden: true,
@@ -36,10 +42,14 @@ const includeExtras = urlParams.get('extra') === 'true' ? true : false
 
 const routes = [...s2025, ...sLOM]
   .filter(route => (includeExtras ? true : route.hidden !== true))
-  .map(route => ({
-    name: route.name,
-    url: `../${route.url}.html`,
-    icon: route.icon,
-  }))
+  .map(route => {
+    return route.separator
+      ? { separator: true }
+      : {
+          name: route.name,
+          url: `../${route.url}.html${queryString}`,
+          icon: route.icon,
+        }
+  })
 
 export { routes }
