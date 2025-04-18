e751050fafe461f357fb71d92e1837e348f6f588
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-04-18 11:34:51 -0700

    global: add babel locally

--- 0_global/modules/react/client.js
+++
@@ -1,4 +0,0 @@
-/* esm.sh - react-dom@18.3.1/client */
-import "/react-dom@18.3.1/es2022/react-dom.mjs";
-export * from "/react-dom@18.3.1/es2022/client.mjs";
-export { default } from "/react-dom@18.3.1/es2022/client.mjs";
