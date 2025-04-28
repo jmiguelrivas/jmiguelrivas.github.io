08b7fdae27a01120fd171a68b39f8a538827e10b
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-04-28 06:45:34 -0700

    lom: add sea users

--- lom/js/db_users-sea.js
+++ lom/js/db_users-sea.js
@@ -1,11 +1,314 @@
 import { User } from './classes.js'
 
 export const sea = [
-  // new User(['Prophet'], ['EN_122'], 'A0607', [
-  //   { rank: 'top', position: 1, date: '2025-03-03' },
-  // ]),
   new User(['Yacz'], ['EN_040'], 'C100D', [
     { rank: 'creator', date: '2025-04-25' },
     { rank: 'elite', date: '2025-04-19' },
   ]),
+
+  new User(['Prophet'], ['EN_122'], 'A0607', [
+    { rank: 'top', position: 1, date: '2025-04-28' },
+  ]),
+  new User(['Mini'], ['EN_021'], '6010B', [
+    { rank: 'top', position: 2, date: '2025-04-28' },
+  ]),
+  new User(['Zenos'], ['VN_042'], '40407', [
+    { rank: 'top', position: 3, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_004'], '31604', [
+    { rank: 'top', position: 4, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_004'], 'C0C09', [
+    { rank: 'top', position: 5, date: '2025-04-28' },
+  ]),
+  new User(['Mernyl'], ['TH_002'], '31E09', [
+    { rank: 'top', position: 6, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_004'], '60605', [
+    { rank: 'top', position: 7, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_047'], 'C0F03', [
+    { rank: 'top', position: 8, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_047'], '5000F', [
+    { rank: 'top', position: 9, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_004'], 'B010D', [
+    { rank: 'top', position: 10, date: '2025-04-28' },
+  ]),
+  new User(['SteveChong'], ['EN_020'], '40C0A', [
+    { rank: 'top', position: 11, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_021'], '70A0B', [
+    { rank: 'top', position: 12, date: '2025-04-28' },
+  ]),
+  new User(['Hika'], ['EN_004'], 'E020E', [
+    { rank: 'top', position: 13, date: '2025-04-28' },
+  ]),
+  new User(['Wiggly'], ['EN_071'], '71406', [
+    { rank: 'top', position: 14, date: '2025-04-28' },
+  ]),
+  new User(['DavLee'], ['EN_047'], '40602', [
+    { rank: 'top', position: 15, date: '2025-04-28' },
+  ]),
+  new User(['MasonMount'], ['TH_274'], 'C1301', [
+    { rank: 'top', position: 16, date: '2025-04-28' },
+  ]),
+  new User(['Oliver'], ['EN_021'], 'B0C07', [
+    { rank: 'top', position: 17, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_047'], '4020F', [
+    { rank: 'top', position: 18, date: '2025-04-28' },
+  ]),
+  new User(['abcdefgh'], ['TH_069'], '80708', [
+    { rank: 'top', position: 19, date: '2025-04-28' },
+  ]),
+  new User(['Mrlwihsnrle'], ['TH_364'], '61500', [
+    { rank: 'top', position: 20, date: '2025-04-28' },
+  ]),
+  new User(['BanhBeo', 'lu_chang'], ['TH_237'], '41209', [
+    { rank: 'top', position: 21, date: '2025-04-28' },
+  ]),
+  new User(['Miraiu', 'Guard'], ['TH_119'], 'C1C02', [
+    { rank: 'top', position: 22, date: '2025-04-28' },
+  ]),
+  new User(['Trang_Ang'], ['TH_427'], '71A0A', [
+    { rank: 'top', position: 23, date: '2025-04-28' },
+  ]),
+  new User(['Trader89'], ['TH_004'], 'E050C', [
+    { rank: 'top', position: 24, date: '2025-04-28' },
+  ]),
+  new User(['VyXinhGai'], ['EN_099'], '50606', [
+    { rank: 'top', position: 25, date: '2025-04-28' },
+  ]),
+  new User(['Cut_Nho'], ['TH_001'], '01E0F', [
+    { rank: 'top', position: 26, date: '2025-04-28' },
+  ]),
+  new User(['Doris'], ['EN_223'], 'B090A', [
+    { rank: 'top', position: 27, date: '2025-04-28' },
+  ]),
+  new User(['Ryo'], ['TH_072'], '91E09', [
+    { rank: 'top', position: 28, date: '2025-04-28' },
+  ]),
+  new User(['Zerstore'], ['TH_196'], 'F090E', [
+    { rank: 'top', position: 29, date: '2025-04-28' },
+  ]),
+  new User(['Zoo Bong'], ['TH_121'], '41901', [
+    { rank: 'top', position: 30, date: '2025-04-28' },
+  ]),
+  new User(['Gambino'], ['VN_047'], '71702', [
+    { rank: 'top', position: 31, date: '2025-04-28' },
+  ]),
+  new User(['Vyt_iuu'], ['TH_072'], 'B0106', [
+    { rank: 'top', position: 32, date: '2025-04-28' },
+  ]),
+  new User(['DojOnline'], ['TH_066'], 'E0903', [
+    { rank: 'top', position: 33, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_001'], '9130F', [
+    { rank: 'top', position: 34, date: '2025-04-28' },
+  ]),
+  new User(['Pho'], ['TH_195'], '0100D', [
+    { rank: 'top', position: 35, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_071'], '41F06', [
+    { rank: 'top', position: 36, date: '2025-04-28' },
+  ]),
+  new User(['Ghastly'], ['EN_048'], 'B1D0D', [
+    { rank: 'top', position: 37, date: '2025-04-28' },
+  ]),
+  new User(['Sua'], ['TH_001'], '71707', [
+    { rank: 'top', position: 38, date: '2025-04-28' },
+  ]),
+  new User(['Nam_Dui_Ga'], ['TH_001'], '1100A', [
+    { rank: 'top', position: 39, date: '2025-04-28' },
+  ]),
+  new User(['ClonePudding'], ['TH_315'], '1180B', [
+    { rank: 'top', position: 40, date: '2025-04-28' },
+  ]),
+  new User(['Yoyo'], ['TH_072'], '90D02', [
+    { rank: 'top', position: 41, date: '2025-04-28' },
+  ]),
+  new User(['Brown'], ['EN_089'], 'B0109', [
+    { rank: 'top', position: 42, date: '2025-04-28' },
+  ]),
+  new User(['Seal'], ['EN_001'], '12305', [
+    { rank: 'top', position: 43, date: '2025-04-28' },
+  ]),
+  new User(['Cao'], ['TH_072'], '80B0F', [
+    { rank: 'top', position: 44, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['TH_120'], 'D1207', [
+    { rank: 'top', position: 45, date: '2025-04-28' },
+  ]),
+  new User(['zJDz'], ['TH_003'], '01F05', [
+    { rank: 'top', position: 46, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_004'], '7000E', [
+    { rank: 'top', position: 47, date: '2025-04-28' },
+  ]),
+  new User(['BanhBaoO'], ['TH_339'], '31404', [
+    { rank: 'top', position: 48, date: '2025-04-28' },
+  ]),
+  new User(['YuriYumim'], ['VN_084'], '61D08', [
+    { rank: 'top', position: 49, date: '2025-04-28' },
+  ]),
+  new User(['Giant'], ['VN_042'], 'A0B03', [
+    { rank: 'top', position: 50, date: '2025-04-28' },
+  ]),
+  new User(['Mr_Sam'], ['TH_121'], '8070E', [
+    { rank: 'top', position: 51, date: '2025-04-28' },
+  ]),
+  new User(['DevsAteHorse'], ['TH_364'], 'B0209', [
+    { rank: 'top', position: 52, date: '2025-04-28' },
+  ]),
+  new User(['TheDoctor88'], ['EN_089'], 'B0D0C', [
+    { rank: 'top', position: 53, date: '2025-04-28' },
+  ]),
+  new User(['Rei'], ['EN_141'], '21F02', [
+    { rank: 'top', position: 54, date: '2025-04-28' },
+  ]),
+  new User(['Thien'], ['TH_120'], '8180F', [
+    { rank: 'top', position: 55, date: '2025-04-28' },
+  ]),
+  new User(['NineHair'], ['EN_126'], '8060D', [
+    { rank: 'top', position: 56, date: '2025-04-28' },
+  ]),
+  new User(['Casp'], ['EN_099'], '6020D', [
+    { rank: 'top', position: 57, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_001'], '61C0C', [
+    { rank: 'top', position: 58, date: '2025-04-28' },
+  ]),
+  new User(['PorkAdobo'], ['EN_099'], '91D0C', [
+    { rank: 'top', position: 59, date: '2025-04-28' },
+  ]),
+  new User(['Nam_truffle'], ['TH_497'], '40508', [
+    { rank: 'top', position: 60, date: '2025-04-28' },
+  ]),
+  new User(['PeNa'], ['TH_072'], 'C0809', [
+    { rank: 'top', position: 61, date: '2025-04-28' },
+  ]),
+  new User(['BiiuBiiu'], ['TH_364'], '91602', [
+    { rank: 'top', position: 62, date: '2025-04-28' },
+  ]),
+  new User(['MaYomYee'], ['EN_089'], '7140E', [
+    { rank: 'top', position: 63, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['TH_002'], '80204', [
+    { rank: 'top', position: 64, date: '2025-04-28' },
+  ]),
+  new User(['Kirs'], ['EN_004'], 'D0409', [
+    { rank: 'top', position: 65, date: '2025-04-28' },
+  ]),
+  new User(['Muc'], ['TH_001'], '50F07', [
+    { rank: 'top', position: 66, date: '2025-04-28' },
+  ]),
+  new User(['Croy'], ['TH_476'], '80B0A', [
+    { rank: 'top', position: 67, date: '2025-04-28' },
+  ]),
+  new User(['Friendly'], ['VN_084'], 'A110C', [
+    { rank: 'top', position: 68, date: '2025-04-28' },
+  ]),
+  new User(['TheDesertFox'], ['TH_066'], '31508', [
+    { rank: 'top', position: 69, date: '2025-04-28' },
+  ]),
+  new User(['Seal'], ['TH_237'], '30F08', [
+    { rank: 'top', position: 70, date: '2025-04-28' },
+  ]),
+  new User(['Dun'], ['EN_132'], 'C0500', [
+    { rank: 'top', position: 71, date: '2025-04-28' },
+  ]),
+  new User(['RCB'], ['TH_001'], '01F08', [
+    { rank: 'top', position: 72, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['TH_192'], '21001', [
+    { rank: 'top', position: 73, date: '2025-04-28' },
+  ]),
+  new User(['6ix9ine'], ['VN_001'], '5190D', [
+    { rank: 'top', position: 74, date: '2025-04-28' },
+  ]),
+  new User(['thebeach'], ['EN_165'], '50104', [
+    { rank: 'top', position: 75, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_089'], '60D04', [
+    { rank: 'top', position: 76, date: '2025-04-28' },
+  ]),
+  new User(['(BLANK)'], ['VN_115'], 'E050C', [
+    { rank: 'top', position: 77, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_089'], '61E02', [
+    { rank: 'top', position: 78, date: '2025-04-28' },
+  ]),
+  new User(['TaThiVic'], ['TH_072'], '51301', [
+    { rank: 'top', position: 79, date: '2025-04-28' },
+  ]),
+  new User(['Zany'], ['EN_126'], '40906', [
+    { rank: 'top', position: 80, date: '2025-04-28' },
+  ]),
+  new User(['AzumaLong'], ['TH_072'], '70806', [
+    { rank: 'top', position: 81, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_047'], '01307', [
+    { rank: 'top', position: 82, date: '2025-04-28' },
+  ]),
+  new User(['Saitama'], ['TH_072'], 'D0204', [
+    { rank: 'top', position: 83, date: '2025-04-28' },
+  ]),
+  new User(['KoPeD'], ['EN_020'], '50508', [
+    { rank: 'top', position: 84, date: '2025-04-28' },
+  ]),
+  new User(['Hai_ass_Trug'], ['TH_286'], '70303', [
+    { rank: 'top', position: 85, date: '2025-04-28' },
+  ]),
+  new User(['HairVaan'], ['TH_315'], '51E0F', [
+    { rank: 'top', position: 86, date: '2025-04-28' },
+  ]),
+  new User(['Tu_Linh'], ['TH_003'], '60002', [
+    { rank: 'top', position: 87, date: '2025-04-28' },
+  ]),
+  new User(['ShiMi'], ['TH_222'], 'F070D', [
+    { rank: 'top', position: 88, date: '2025-04-28' },
+  ]),
+  new User(['Killlllll'], ['TH_120'], 'B0D01', [
+    { rank: 'top', position: 89, date: '2025-04-28' },
+  ]),
+  new User(['MeijMemories'], ['TH_121'], 'A1804', [
+    { rank: 'top', position: 90, date: '2025-04-28' },
+  ]),
+  new User(['(?)'], ['EN_047'], '6010D', [
+    { rank: 'top', position: 91, date: '2025-04-28' },
+  ]),
+  new User(['KuDen'], ['TH_238'], '90D0F', [
+    { rank: 'top', position: 92, date: '2025-04-28' },
+  ]),
+  new User(['HarleyQuinn'], ['TH_402'], '40907', [
+    { rank: 'top', position: 93, date: '2025-04-28' },
+  ]),
+  new User(['888888_'], ['EN_118'], '21B0A', [
+    { rank: 'top', position: 94, date: '2025-04-28' },
+  ]),
+  new User(['miyuu'], ['VN_122'], 'A1E04', [
+    { rank: 'top', position: 95, date: '2025-04-28' },
+  ]),
+  new User(['Andre_Onana'], ['TH_274'], 'F0004', [
+    { rank: 'top', position: 96, date: '2025-04-28' },
+  ]),
+  new User(['Ancomchua'], ['TH_196'], 'A0F0E', [
+    { rank: 'top', position: 97, date: '2025-04-28' },
+  ]),
+  new User(['Den_O_Duong'], ['TH_427'], 'C1004', [
+    { rank: 'top', position: 98, date: '2025-04-28' },
+  ]),
+  new User(['TheOne'], ['EN_089'], 'A0E05', [
+    { rank: 'top', position: 99, date: '2025-04-28' },
+  ]),
+  new User(['SIK ISS'], ['VN_137'], '31F08', [
+    { rank: 'top', position: 100, date: '2025-04-28' },
+  ]),
+
+
+  // new User(['(?)'], [''], '', [
+  //   { rank: 'top', position: , date: '2025-04-28' },
+  // ]),
 ]
