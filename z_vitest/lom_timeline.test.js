df35325f9632ffd43a5c25e66ab5c93e1f7ef8b9
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-03 05:04:29 -0700

    vitest: fix test

--- z_vitest/lom_timeline.test.js
+++ z_vitest/lom_timeline.test.js
@@ -3,17 +3,21 @@ import { servers } from '../lom/js/db_merges.js'
 
 const pt169 = servers.find(server => server.key.label === 'PT-169')
 
-const users = pt169.values
-  .map(server => {
-    return server.users.map(user => user.label)
-  })
+const users = [
+  pt169.key.users.map(user => user.id),
+  ...pt169.values.map(server => {
+    return server.users.map(user => user.id)
+  }),
+]
   .flat()
-  .filter(Boolean)
+  .sort()
+// console.log(users)
 
 const serversList = [
   pt169.key.label,
   ...pt169.values.map(server => server.label),
 ]
+// console.log(serversList)
 
 test('pt-169 servers list verification', () => {
   expect(serversList).toEqual([
@@ -33,21 +37,25 @@ test('pt-169 servers list verification', () => {
 
 test('pt-169 users list verification', () => {
   expect(users).toEqual([
-    'E0F0B :: BabaYaga',
-    '81302 :: Benimaru',
-    '8010A :: Luffy',
-    '40301 :: Doruk',
-    '4010F :: Hakan',
-    'C080F :: kirito',
-    '90709 :: LimRabei',
-    '8090b :: Ummabel',
-    '31003 :: Fenix',
-    'B0D03 :: Migatte',
-    '71B0B :: Colt75',
-    '4040C :: Goddess',
-    'C0400 :: n17',
-    '2190B :: Gogeta',
-    '40501 :: Pluton',
-    '7080D :: Luxury',
+    '11F08',
+    '2190B',
+    '31003',
+    '4010F',
+    '40301',
+    '4040C',
+    '40501',
+    '7080D',
+    '71B0B',
+    '8010A',
+    '8090b',
+    '81302',
+    '90709',
+    'A0107',
+    'B0D03',
+    'C0400',
+    'C0508',
+    'C0800',
+    'C080F',
+    'E0F0B',
   ])
 })
