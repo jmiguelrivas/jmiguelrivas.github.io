563b4e05b4c247cbf20474a7399fe3e8d31e155f
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-06 13:21:59 -0700

    challenge: string reverse

--- storybook_dev/challenges-js/StringReverse.stories.js
+++ storybook_dev/challenges-js/StringReverse.stories.js
@@ -10,43 +10,33 @@ export default {
     layout: 'fullscreen',
   },
   args: {
-    str1: 'a gentleman',
-    str2: 'elegant man',
+    str: 'a gentleman',
   },
   argTypes: {
-    str1: {
-      control: 'text',
-    },
-    str2: {
+    str: {
       control: 'text',
     },
   },
 }
-
-function processWord(str) {
-  return str.split('').sort().join('').toLowerCase().replace(/\s*/, '')
-}
-
-function isAnagram(str1, str2) {
-  const s1 = processWord(str1)
-  const s2 = processWord(str2)
-  return s1 === s2
-}
-
-export const Anagram = args => {
+export const StringReverse = args => {
   const {
-    str1,
-    str2
+    str
   } = args
   const container = document.createElement('section')
   container.classList.add('workarea')
 
+  const result = str
+    .split('')
+    .sort(() => -1)
+    .join('')
+
+  console.log(result)
+
   container.innerHTML = /*html*/ `
 <nn-caja padding="4" size="1200">
-  <h1>Anagram Checker</h1>
-  <nn-code>${compressText(`[str1]: "${str1}" = "${processWord(str1)}"`)}</nn-code>
-  <nn-code>${compressText(`[str2]: "${str2}" = "${processWord(str2)}"`)}</nn-code>
-  <nn-code>${compressText(`"${processWord(str1)}" = "${processWord(str2)}" = ${isAnagram(str1, str2)}`)}</nn-code>
+  <h1>String Reverse</h1>
+  <nn-code>${compressText(`${str}`)}</nn-code>
+  <nn-code>${compressText(`${result}`)}</nn-code>
 </nn-caja>
 `
   return container
