51fd41e7adb03c942d0d6f9ff5af0d5533b4be2d
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-13 01:09:05 -0700

    lom: finish migration to a independent repo

--- lom/js/db_users-jp.js
+++
@@ -1,8 +0,0 @@
-import { user } from './classes.js'
-
-export const jp = [
-  //------------------------------- scouts
-  user(['Marcelo'], 'JP_2178', '9070C', [
-    { rank: 'detective', date: '2025-04-25' },
-  ]),
-]
--- lom/js/db_users-tw.js
+++
@@ -1,8 +0,0 @@
-import { user } from './classes.js'
-
-export const tw = [
-  //------------------------------- scouts
-  user(['Pavel'], 'TW_1817', '31802', [
-    { rank: 'detective', date: '2025-04-25' },
-  ]),
-]
